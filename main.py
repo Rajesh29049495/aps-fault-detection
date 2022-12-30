71993b023364546ae7c84b8b938d854dd393c6d7
Rajesh29049495 <rajesh29049495@gmail.com>
2022-12-30 09:44:57 +0530

    data ingestion done

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
 import os,sys
 from sensor.entity import config_entity
-from sensor import components
+from sensor.components import data_ingestion
 
 
 
@@ -13,7 +13,7 @@ if __name__=="__main__":
         training_pipeline_config = config_entity.TrainingPipelineConfig()
         data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         print(data_ingestion_config.to_dict())
-        data_ingestion = DataIngestion(data_ingestion_config= data_ingestion_config)
+        data_ingestion = data_ingestion.DataIngestion(data_ingestion_config= data_ingestion_config)
         print(data_ingestion.initiate_data_ingestion())
 
     except Exception as e:
--- sensor/__init__.py
+++ sensor/__init__.py
@@ -1,3 +1,3 @@
 from dotenv import load_dotenv
-print(f"loading environment varaible from .env file")  #this message will be printed whenever this environment variable reading will be done
+print(f"loading environment variable from .env file")  #this message will be printed whenever this environment variable reading will be done
 load_dotenv()
